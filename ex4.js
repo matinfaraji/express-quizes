b980d4ea8161262a44fc7e2ba2821cb02acea95e
matin faraji <matinfy1380@gmail.com>
2024-04-24 01:22:07 +0330

    ex4 solved

--- ex4.js
+++ ex4.js
@@ -1,3 +1,19 @@
 //Create a /weird route that passes name and title parameters to the app and prints the name and title parameters to the screen.
 
 //Answer
+const express = require("express");
+
+const app = express();
+
+app.use("/search",(req,res)=>{
+    const{name , title}=req.query
+   console.log(name +"  " + title); 
+    res.send(name +"  " + title)
+})
+
+
+
+const PORT = 3001;
+app.listen(PORT,()=>{
+    console.log(`server is runing on url :  http://localhost:${PORT}`);
+})
