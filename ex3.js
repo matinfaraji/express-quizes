bcc14ff7de3fb6b2e1f18f4b590bd1f7333b2073
matin faraji <matinfy1380@gmail.com>
2024-04-24 01:28:07 +0330

    ex3 solved

--- ex3.js
+++ ex3.js
@@ -2,3 +2,18 @@
 // hint: use req.params
 
 //Answer
+const express = require("express");
+
+const app = express();
+
+app.use("/r/:anything",(req,res)=>{
+   const {anything}=req.params
+    res.send(anything)
+})
+
+
+
+const PORT = 3001;
+app.listen(PORT,()=>{
+    console.log(`server is runing on url :  http://localhost:${PORT}`);
+})
